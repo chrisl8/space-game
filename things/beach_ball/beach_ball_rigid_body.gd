6ce9ac2833d7bca4d7ea0559c23ef24bc7c40795
Christen Lofland <christen@lofland.net>
2023-08-23 11:51:24 -0500

    Clean up beach ball script.

--- things/beach_ball/beach_ball_rigid_body.gd
+++ things/beach_ball/beach_ball_rigid_body.gd
@@ -1,44 +1,17 @@
 extends RigidBody3D
 
-#@export var max_linear_velocity = 20
 @export var bounds_distance = 100
 
-#var lienar_velocity_x_abs   = 0
-#var lienar_velocity_y_abs   = 0
-#var lienar_velocity_z_abs   = 0
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Only process for the local player
-	set_physics_process(get_multiplayer_authority() == multiplayer.get_unique_id())
+	set_physics_process(User.is_server)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	# Only the server should act on this object, as the server owns it,
 	# especially the delete part.
-	if User.is_server:
-		# Delete if it gets out of bounds
-		if abs(position.x) > bounds_distance:
-			get_parent().queue_free()
-		if abs(position.y) > bounds_distance:
-			get_parent().queue_free()
-		if abs(position.z) > bounds_distance:
-			get_parent().queue_free()
-
-		# Clamp maximum velocity
-#		if linear_velocity.x > max_linear_velocity:
-#			linear_velocity.x = max_linear_velocity
-#		if linear_velocity.x < -max_linear_velocity:
-#			linear_velocity.x = -max_linear_velocity
-#
-#		if linear_velocity.y > max_linear_velocity:
-#			linear_velocity.y = max_linear_velocity
-#		if linear_velocity.y < -max_linear_velocity:
-#			linear_velocity.y = -max_linear_velocity
-#
-#		if linear_velocity.z > max_linear_velocity:
-#			linear_velocity.z = max_linear_velocity
-#		if linear_velocity.z < -max_linear_velocity:
-#			linear_velocity.z = -max_linear_velocity
+	# Delete if it gets out of bounds
+	if abs(position.x) > bounds_distance:
+		get_parent().queue_free()
+	if abs(position.y) > bounds_distance:
+		get_parent().queue_free()
+	if abs(position.z) > bounds_distance:
+		get_parent().queue_free()
