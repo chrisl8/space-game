2b9c87778ea9d2322f3e1f2378ad44ae13d56ad1
Christen Lofland <christen@lofland.net>
2023-09-16 16:08:08 -0500

    Improve music volume and logic.

--- global_variables.gd
+++ global_variables.gd
@@ -4,3 +4,4 @@ var is_server: bool = false
 var user_name: String = ""
 var player_id: int = -1
 var local_debug_instance_number: int = -1
+var intro_music_has_played: bool = false
--- spaceship/spaceship.gd
+++ spaceship/spaceship.gd
@@ -20,6 +20,10 @@ func _ready() -> void:
 		get_node("/root").add_child(toast)
 		toast.show()
 
+	if not Globals.is_server and not Globals.intro_music_has_played:
+		Globals.intro_music_has_played = true
+		$AudioStreamPlayer.play()
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
