7964f0c5c913d55081bb46c0f055d1694c709375
Christen Lofland <christen@lofland.net>
2023-10-29 20:47:16 -0500

    Convert vending machine buttons to multiplayer, add text on screen and replace screen.

--- things/vending_machine/vending_machine.gd
+++ things/vending_machine/vending_machine.gd
@@ -20,34 +20,54 @@ var button_panel_labels: Dictionary = {
 
 
 func _ready() -> void:
+	set_screen_text("Enter Code\nto\nVEND\nstuff")
 	var button_nodes: Array = get_node("./root/Left Panel016/").get_children()
 	for button_node: Node3D in button_nodes:
 		if button_panel_labels.has(button_node.name) and button_node.has_node("StaticBody3D"):
 			var button_static_body_node: Node3D = button_node.get_node("StaticBody3D")
 			# Inset all activated buttons slightly to start with.
 			button_node.position.z = button_node.position.z - 0.01
-			var original_button_position: Vector3 = button_static_body_node.position
-			button_static_body_node.input_event.connect(
-				_on_static_body_3d_input_event.bind(
-					button_panel_labels[button_node.name].id,
-					button_node.get_path(),
-					original_button_position
+			button_panel_labels[button_node.name].original_button_position = (
+				button_static_body_node.position
+			)
+			(
+				button_static_body_node
+				. input_event
+				. connect(
+					(
+						_on_static_body_3d_input_event
+						. bind(
+							button_node.name,
+							button_panel_labels[button_node.name].id,
+							button_node.get_path(),
+						)
+					)
 				)
 			)
-			print(button_node.name, " ", button_panel_labels[button_node.name].id)
 
 
-# 	var test_node: Node3D = get_node("./root/Left Panel016/Left Panel011")
+@rpc() func depress_button(button_node_name: String, button_node_path: NodePath) -> void:
+	var button_node: Node = get_node_or_null(button_node_path)
+	if button_node:
+		# TODO: this should happen on the server which means:
+		# RPC calls
+		# Server controls position and position is synced
+		# Server does the needful in response to the button input
+		button_node.position.z = (
+			button_panel_labels[button_node_name].original_button_position.z - 0.02
+		)
+		await get_tree().create_timer(0.25).timeout
+		button_node.position.z = (
+			button_panel_labels[button_node_name].original_button_position.z - 0.01
+		)
 
-# 	get_node("./root/Left Panel016/Left Panel011/StaticBody3D").input_event.connect(
-# 		_on_static_body_3d_input_event.bind(1, test_node.get_path(), original_button_position)
-# 	)
-# #	get_node("./root/Left Panel016/Left Panel006/StaticBody3D").input_event.connect(
-# #		_on_static_body_3d_input_event.bind(2)
-# #	)
-# 	get_node("./root/Left Panel016/Left Panel001/StaticBody3D").input_event.connect(
-# 		_on_static_body_3d_input_event.bind(15)
-# 	)
+
+@rpc("any_peer") func server_button_clicked(
+	button_node_name: String, button_index: int, button_node_path: NodePath
+) -> void:
+	if Globals.is_server:
+		depress_button.rpc(button_node_name, button_node_path)
+		set_screen_text.rpc(str(button_index))
 
 
 func _on_static_body_3d_input_event(
@@ -56,34 +76,15 @@ func _on_static_body_3d_input_event(
 	_position: Vector3,
 	_normal: Vector3,
 	_shape_idx: int,
+	button_node_name: String,
 	button_index: int,
 	button_node_path: NodePath,
-	original_button_position: Vector3
 ) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("Left Mouse Button Clicked on Button ", button_node_path, " ", button_index)
-			var button_node: Node = get_node_or_null(button_node_path)
-			if button_node:
-				# TODO: this should happen on the server which means:
-				# RPC calls
-				# Server controls position and position is synced
-				# Server does the needful in response to the button input
-				button_node.position.z = original_button_position.z - 0.02
-				await get_tree().create_timer(0.25).timeout
-				button_node.position.z = original_button_position.z - 0.01
+			server_button_clicked.rpc(button_node_name, button_index, button_node_path)
 
-		# if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == false:
-		# 	print("Left Mouse Button Released on Button ", button_node_path, " ", button_index)
-		# if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed == true:
-		# 	print("Right Mouse Button Clicked on Button ", button_node_path, " ", button_index)
-		# if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed == false:
-		# 	print("Right Mouse Button Released on Button ", button_node_path, " ", button_index)
-		# if event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed == true:
-		# 	print("Middle Mouse Button Clicked on Button ", button_node_path, " ", button_index)
-		# if event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed == false:
-		# 	print("Middle Mouse Button Released on Button ", button_node_path, " ", button_index)
-		# if event.button_index == MOUSE_BUTTON_LEFT and event.double_click == true:
-		# 	print(
-		# 		"Left Mouse Button Double Clicked on Button ", button_node_path, " ", button_index
-		# 	)
+
+@rpc() func set_screen_text(text: String) -> void:
+	var screen: Node3D = get_node("./root/Left Panel016/Screen/Screen Text")
+	screen.text = text
