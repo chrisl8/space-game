5268fb5001d80023ffc2ca57b7e8e497e702131b
Christen Lofland <christen@lofland.net>
2023-11-07 12:20:38 -0600

    Updated vending machine model import after organizing meshes in Blender

--- things/vending_machine/vending_machine.gd
+++ things/vending_machine/vending_machine.gd
@@ -1,27 +1,27 @@
 extends Node3D
 
 var button_panel_labels: Dictionary = {
-	"Left Panel001": {"id": 15},
-	"Left Panel002": {"id": 5},
-	"Left Panel003": {"id": 11},
-	"Left Panel004": {"id": 13},
-	"Left Panel005": {"id": 4},
-	"Left Panel006": {"id": 2},
-	"Left Panel007": {"id": 10},
-	"Left Panel008": {"id": 7},
-	"Left Panel009": {"id": 6},
-	"Left Panel010": {"id": 8},
-	"Left Panel011": {"id": 1},
-	"Left Panel012": {"id": 14},
-	"Left Panel013": {"id": 3},
-	"Left Panel014": {"id": 9},
-	"Left Panel015": {"id": 12},
+	"Button01": {"id": 1},
+	"Button02": {"id": 2},
+	"Button03": {"id": 3},
+	"Button04": {"id": 4},
+	"Button05": {"id": 5},
+	"Button06": {"id": 6},
+	"Button07": {"id": 7},
+	"Button08": {"id": 8},
+	"Button09": {"id": 9},
+	"Button10": {"id": 10},
+	"Button11": {"id": 11},
+	"Button12": {"id": 12},
+	"Button13": {"id": 13},
+	"Button14": {"id": 14},
+	"Button15": {"id": 15},
 }
 
 
 func _ready() -> void:
 	set_screen_text("Enter Code\nto\nVEND\nstuff")
-	var button_nodes: Array = get_node("./root/Left Panel016/").get_children()
+	var button_nodes: Array = get_node("VendingMachineMesh/Left Panel").get_children()
 	for button_node: Node3D in button_nodes:
 		if button_panel_labels.has(button_node.name) and button_node.has_node("StaticBody3D"):
 			var button_static_body_node: Node3D = button_node.get_node("StaticBody3D")
@@ -49,10 +49,6 @@ func _ready() -> void:
 @rpc() func depress_button(button_node_name: String, button_node_path: NodePath) -> void:
 	var button_node: Node = get_node_or_null(button_node_path)
 	if button_node:
-		# TODO: this should happen on the server which means:
-		# RPC calls
-		# Server controls position and position is synced
-		# Server does the needful in response to the button input
 		button_node.position.z = (
 			button_panel_labels[button_node_name].original_button_position.z - 0.02
 		)
@@ -86,5 +82,5 @@ func _on_static_body_3d_input_event(
 
 
 @rpc() func set_screen_text(text: String) -> void:
-	var screen: Node3D = get_node("./root/Left Panel016/Screen/Screen Text")
+	var screen: Node3D = get_node_or_null("Screen Text")
 	screen.text = text
