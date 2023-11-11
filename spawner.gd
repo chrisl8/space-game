faf7ea7cdf46ecb78f7c80ba03e536608c361203
Christen Lofland <christen@lofland.net>
2023-11-11 17:12:57 -0600

    Refactor spawner into reusable functions

--- spawner.gd
+++ spawner.gd
@@ -2,6 +2,10 @@ extends Node
 
 var done_once: bool = false
 var Chair: Resource = preload("res://things/chair/chair.tscn")
+var BeachBall: Resource = preload("res://things/beach_ball/beach_ball.tscn")
+var Fish: Resource = preload("res://things/fish/fish.tscn")
+var Floater: Resource = preload("res://things/Floater/Floater.tscn")
+var PlantA: Resource = preload("res://things/plant_a/plant_a.tscn")
 
 @onready var things_spawning_node: Node = get_node("../Main/Things")
 
@@ -15,67 +19,46 @@ var Chair: Resource = preload("res://things/chair/chair.tscn")
 		things_spawning_node.add_child(new_thing, true)
 
 
-func things() -> void:
-	# Ball
-	var thing_name_to_spawn: String = "Ball01"
-	var BeachBall: Resource = preload("res://things/beach_ball/beach_ball.tscn")
+func thing(thing_name: String, id: int) -> void:
+	var thing_name_to_spawn: String = str(thing_name, "-", id)
 	var existing_thing: Node = things_spawning_node.get_node_or_null(thing_name_to_spawn)
 	if not existing_thing:
-		var new_thing: Node = BeachBall.instantiate()
+		var new_thing: Node
+		match thing_name:
+			"Ball":
+				new_thing = BeachBall.instantiate()
+			"Fish":
+				new_thing = Fish.instantiate()
+			"Floater":
+				new_thing = Floater.instantiate()
+			"PlantA":
+				new_thing = PlantA.instantiate()
+			"Chair":
+				new_thing = Chair.instantiate()
+			_:
+				Helpers.log_print(str("Invalid thing to spawn name: ", thing_name), "red")
+				return
 		new_thing.name = str(thing_name_to_spawn)
-		Helpers.log_print(str("spawning ", thing_name_to_spawn))
+		Helpers.log_print(str("spawning ", thing_name_to_spawn), "yellow")
 		things_spawning_node.add_child(new_thing)
 
-	thing_name_to_spawn = "Ball02"
-	existing_thing = things_spawning_node.get_node_or_null(thing_name_to_spawn)
-	if not existing_thing:
-		var new_thing: Node = BeachBall.instantiate()
-		new_thing.name = str(thing_name_to_spawn)
-		Helpers.log_print(str("spawning ", thing_name_to_spawn))
-		things_spawning_node.add_child(new_thing)
 
-	# Spawn a test fish
-	thing_name_to_spawn = "Fish"
-	var Fish: Resource = preload("res://things/fish/fish.tscn")
-	existing_thing = things_spawning_node.get_node_or_null(thing_name_to_spawn)
-	if not existing_thing:
-		var new_thing: Node = Fish.instantiate()
-		new_thing.name = str(thing_name_to_spawn)
-		Helpers.log_print(str("spawning ", thing_name_to_spawn))
-		# new_thing.position = Vector3(4, 1, -2)
-		things_spawning_node.add_child(new_thing)
+func things() -> void:
+	# Balls
+	thing("Ball", 1)
+	thing("Ball", 2)
+	thing("Fish", 1)
+	thing("Floater", 1)
 
+	# Only spawn the chair once, even if it goes away
+	# so that it is not re-spawned when picked up
 	if not done_once:
-		# TODO: During testing, insure this doesn't respawn when we "pick it up"
 		done_once = true
-		thing_name_to_spawn = "Chair01"
-		existing_thing = things_spawning_node.get_node_or_null(thing_name_to_spawn)
-		if not existing_thing:
-			var new_thing: Node = Chair.instantiate()
-			new_thing.name = str(thing_name_to_spawn)
-			# TODO: I wish I could locate items when I create them, but I can't. No idea why.
-			# new_thing.position = Vector3(8, 1, -8)
-			# new_thing.rotation.y = -45.0
-			things_spawning_node.add_child(new_thing)
+		thing("Chair", 1)
 
 	#Spawn randomization bounds configured for 3
 	#Will generate somewhat reasonably up to 20
 	var plants_to_spawn: int = 3
-
-	var Plant: Resource = preload("res://things/plant_a/plant_a.tscn")
 	while plants_to_spawn > 0:
-		thing_name_to_spawn = "Plant_A" + str(plants_to_spawn)
-		existing_thing = things_spawning_node.get_node_or_null(thing_name_to_spawn)
-		if not existing_thing:
-			var new_thing: Node = Plant.instantiate()
-			new_thing.name = str(thing_name_to_spawn)
-			things_spawning_node.add_child(new_thing)
+		thing("PlantA", plants_to_spawn)
 		plants_to_spawn -= 1
-
-	thing_name_to_spawn = "Floater"
-	existing_thing = things_spawning_node.get_node_or_null(thing_name_to_spawn)
-	var Floater: Resource = preload("res://things/Floater/Floater.tscn")
-	if not existing_thing:
-		var new_thing: Node = Floater.instantiate()
-		new_thing.name = str(thing_name_to_spawn)
-		things_spawning_node.add_child(new_thing)
