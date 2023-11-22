76fe60585999d09dd03b1a2745805600ef2800e7
Christen Lofland <christen@lofland.net>
2023-11-22 09:32:28 -0600

    Lint and format

--- things/Floater/FloaterController.gd
+++ things/Floater/FloaterController.gd
@@ -7,12 +7,14 @@ extends RigidBody3D
 
 var current_time: float = 0.0
 var force_applied: bool = false
-var last_position: Vector3 = Vector3(0,0,0)
+var last_position: Vector3 = Vector3(0, 0, 0)
 var current_eye_rot_time: float = 0
 var current_arm_rot_time: float = 0
 var arm_movement_last_rotation: Vector3 = Vector3.ZERO
+var particle_last_position: Vector3 = Vector3.ZERO
 
 @onready var eye: Node3D = $Eye
+
 
 func _ready() -> void:
 	set_physics_process(get_multiplayer_authority() == multiplayer.get_unique_id())
@@ -24,7 +26,7 @@ func _ready() -> void:
 		position = spawn_position
 
 	last_position = position
-	ParticleLastPosition = position
+	particle_last_position = position
 
 
 func process_arms(delta: float) -> void:
@@ -39,19 +41,22 @@ func process_arms(delta: float) -> void:
 	#	target_rotation = Vector3.ZERO
 	#var LerpTarget = arm_movement_last_rotation.lerp(target_rotation,delta*99)
 
-	last_position = lerp(last_position,position,1.5*delta)
-	last_position = Vector3(last_position.x,position.y-2,last_position.z)
+	last_position = lerp(last_position, position, 1.5 * delta)
+	last_position = Vector3(last_position.x, position.y - 2, last_position.z)
 
-	ParticleLastPosition = lerp(ParticleLastPosition,position,0.7*delta)
+	particle_last_position = lerp(particle_last_position, position, 0.7 * delta)
 
 	for arm: Node3D in arms:
 		arm.look_at(last_position, Vector3.UP, false)
 
-	for particlesystem : GPUParticles3D in arm_particles:
-		particlesystem.amount_ratio = clamp(abs((ParticleLastPosition - position).length()*2),0.1,3)
-		particlesystem.speed_scale = clamp(abs((ParticleLastPosition - position).length()*2),1,3)
+	for particle_system: GPUParticles3D in arm_particles:
+		particle_system.amount_ratio = clamp(
+			abs((particle_last_position - position).length() * 2), 0.1, 3
+		)
+		particle_system.speed_scale = clamp(
+			abs((particle_last_position - position).length() * 2), 1, 3
+		)
 
-var ParticleLastPosition = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Delete if it gets out of bounds
@@ -89,7 +94,6 @@ func _physics_process(delta: float) -> void:
 
 
 func _process(delta: float) -> void:
-	
 	# Eye Movement
 	current_eye_rot_time += delta
 	if current_eye_rot_time > 3:
