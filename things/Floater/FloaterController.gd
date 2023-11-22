01c31f23147a32c7e91a5bbf529801361866f763
LiquidMintLeaf <111805837+MinntLeaf@users.noreply.github.com>
2023-11-21 20:50:58 -0600

    Fixed floater arm look_at logic

--- things/Floater/FloaterController.gd
+++ things/Floater/FloaterController.gd
@@ -7,13 +7,12 @@ extends RigidBody3D
 
 var current_time: float = 0.0
 var force_applied: bool = false
-var last_position: Vector3
+var last_position: Vector3 = Vector3(0,0,0)
 var current_eye_rot_time: float = 0
 var current_arm_rot_time: float = 0
 var arm_movement_last_rotation: Vector3 = Vector3.ZERO
 
 @onready var eye: Node3D = $Eye
-
 
 func _ready() -> void:
 	set_physics_process(get_multiplayer_authority() == multiplayer.get_unique_id())
@@ -25,6 +24,7 @@ func _ready() -> void:
 		position = spawn_position
 
 	last_position = position
+	ParticleLastPosition = position
 
 
 func process_arms(delta: float) -> void:
@@ -39,20 +39,19 @@ func process_arms(delta: float) -> void:
 	#	target_rotation = Vector3.ZERO
 	#var LerpTarget = arm_movement_last_rotation.lerp(target_rotation,delta*99)
 
-	#Rotation isn't applied if updated 'too frequently'. Every 0.2 seconds seems to be the most Godot can handle
-	#Maybe the _process function isn't meant for transform changes?
-	if current_arm_rot_time > 0.2:
-		current_arm_rot_time = 0.
-		for arm: Node3D in arms:
-			arm.look_at(last_position)
-		for particlesystem: GPUParticles3D in arm_particles:
-			particlesystem.amount_ratio = clamp(
-				abs((last_position - position).length() * 2), 0.1, 3
-			)
-			particlesystem.speed_scale = clamp(abs((last_position - position).length() * 2), 1, 3)
-		last_position = position
-	#arm_movement_last_rotation = LerpTarget
+	last_position = lerp(last_position,position,1.5*delta)
+	last_position = Vector3(last_position.x,position.y-2,last_position.z)
 
+	ParticleLastPosition = lerp(ParticleLastPosition,position,0.7*delta)
+
+	for arm: Node3D in arms:
+		arm.look_at(last_position, Vector3.UP, false)
+
+	for particlesystem : GPUParticles3D in arm_particles:
+		particlesystem.amount_ratio = clamp(abs((ParticleLastPosition - position).length()*2),0.1,3)
+		particlesystem.speed_scale = clamp(abs((ParticleLastPosition - position).length()*2),1,3)
+
+var ParticleLastPosition = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Delete if it gets out of bounds
@@ -65,7 +64,6 @@ func _physics_process(delta: float) -> void:
 		queue_free()
 
 	current_time += delta
-
 	if current_time > 1 and !force_applied:
 		var min_speed: float = 0.5
 		var max_speed: float = 1
@@ -91,6 +89,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _process(delta: float) -> void:
+	
 	# Eye Movement
 	current_eye_rot_time += delta
 	if current_eye_rot_time > 3:
