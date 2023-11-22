28aca0cb0f7cc0a31b9c69d5d57d35fb91ce7c88
Christen Lofland <christen@lofland.net>
2023-11-21 20:35:45 -0600

    Improve startup and restart scripts.

--- export-helpers/server/restart-server.sh
+++ export-helpers/server/restart-server.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+#echo "${SCRIPT_DIR}" # For debugging
+
+cd "${SCRIPT_DIR}" || exit
 ./space-game.x86_64 --headless -- client shutdown_server
