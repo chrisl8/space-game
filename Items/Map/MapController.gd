fede499a9ccbb430b4748ae7af8bcd3c357435bf
LiquidMintLeaf <111805837+MinntLeaf@users.noreply.github.com>
2024-01-01 22:27:49 -0600

    Tile system development

--- Items/Map/MapController.gd
+++ Items/Map/MapController.gd
@@ -1,11 +1,41 @@
 extends TileMap
 
+@export var SyncedTilePositions: Array[Vector2i]
+@export var SyncedTileIDs: Array[Vector2i]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("AAA")
+	print(GetCellData())
+	print("BBB")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func SetTileData(Positions, IDs):
+	var Count = len(IDs)-1
+	while(Count>=0):
+		set_cell(Positions[Count],IDs[Count])
+		Count-=1
+
+func GetCellData():
+	var Positions: Array[Vector2i]
+	var IDs: Array[Vector2i]
+	Positions = get_used_cells(0)
+
+	for Position in Positions:
+		IDs.append(get_cell_tile_data(0,Position))
+
+	return [Positions,IDs]
+
+func GetCellPositions():
+	return(get_used_cells(0))
+
+func GetCellIDs():
+	var IDs: Array[Vector2i]
+
+	for Position in get_used_cells(0):
+		IDs.append(get_cell_tile_data(0,Position))
+	return(IDs)
