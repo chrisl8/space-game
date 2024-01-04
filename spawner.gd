27bb6024e1693bea8286612c052255cf2dbbf513
Christen Lofland <christen@lofland.net>
2024-01-03 21:08:15 -0600

    Stop spawning and delete 3D things in 2D game.

--- spawner.gd
+++ spawner.gd
@@ -1,10 +1,5 @@
 extends Node
 
-var Chair: Resource = preload("res://things/chair/chair.tscn")
-var Ball: Resource = preload("res://things/ball/ball.tscn")
-var Fish: Resource = preload("res://things/fish/fish.tscn")
-var Floater: Resource = preload("res://things/Floater/Floater.tscn")
-var PlantA: Resource = preload("res://things/plant_a/plant_a.tscn")
 var Map: Resource = preload("res://Items/Map/Map.tscn")
 var done_once: bool = false
 
@@ -24,16 +19,6 @@ func thing(thing_name: String, id: int, spawn_position: Vector3 = Vector3.ZERO) 
 	if not existing_thing:
 		var new_thing: Node
 		match thing_name:
-			"Ball":
-				new_thing = Ball.instantiate()
-			"Fish":
-				new_thing = Fish.instantiate()
-			"Floater":
-				new_thing = Floater.instantiate()
-			"PlantA":
-				new_thing = PlantA.instantiate()
-			"Chair":
-				new_thing = Chair.instantiate()
 			"Map":
 				Helpers.log_print("Spawning Map")
 				new_thing = Map.instantiate()
@@ -54,27 +39,13 @@ func things() -> void:
 	# The way things get lost is physics yeets them out of the rooms
 	# and then they fall past the boundary where they are deleted
 	# by their own code
-	thing("Ball", 1, Vector3(4, 1, -2))
-	thing("Ball", 2, Vector3(4, 1, -2))
-	thing("Floater", 1, Vector3(20.3, 2.4, -3.1))
 
-	# Plants
-	#Spawn randomization bounds configured for 3
-	#Will generate somewhat reasonably up to 20
-	var plants_to_spawn: int = 3
-	while plants_to_spawn > 0:
-		var plant_position: Vector3 = Vector3(
-			2.274 + RandomNumberGenerator.new().randf_range(-1.0, 1.0),
-			0.5,
-			-17.702 + RandomNumberGenerator.new().randf_range(-1.0, 1.0)
-		)
-		thing("PlantA", plants_to_spawn, plant_position)
-		plants_to_spawn -= 1
+	# Nothing left here, so this comment is an example
+	#thing("Ball", 1, Vector3(4, 1, -2))
 
 	# Things to only spawn once, even if they go away
 	# Things that can be picked up will disappear when picked up,
 	# so they must not respawn then.
 	if not done_once:
 		done_once = true
-		thing("Chair", 1, Vector3(8, 1, -8))
-		thing("Map", 1, Vector3(0,0,0))
+		thing("Map", 1, Vector3(0, 0, 0))
