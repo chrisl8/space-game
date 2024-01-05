4555b7bc7cc5e6cc1572336f0ca2a1e274d16736
LiquidMintLeaf <111805837+MinntLeaf@users.noreply.github.com>
2024-01-05 10:49:03 -0600

    Added basic player arm

--- player/PlayerInteractionController.gd
+++ player/PlayerInteractionController.gd
@@ -2,13 +2,17 @@ extends Node2D
 
 var IsLocal: bool = false
 
+@export var Arm: Node2D
+
+@export var ArmRotation: float
+
+var CurrentTool: int = 1
 
 func Initialize(Local: bool):
 	IsLocal = Local
-	set_process(IsLocal)
+	#set_process(IsLocal)
 
 	#
-	set_process(IsLocal)
 	set_process_input(IsLocal)
 	set_process_internal(IsLocal)
 	set_process_unhandled_input(IsLocal)
@@ -17,12 +21,18 @@ func Initialize(Local: bool):
 	set_physics_process_internal(IsLocal)
 	#
 
-
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed(&"interact"):
-		Globals.WorldMap.ModifyCell(
-			Vector2i(randi_range(-50, 50), randi_range(0, -50)), Vector2i(1, 1)
-		)
+	#Need to confirm this works across computers
+	if(IsLocal):
+		MousePosition = get_global_mouse_position()
+		if Input.is_action_just_pressed(&"interact"):
+			Globals.WorldMap.ModifyCell(
+				Vector2i(randi_range(-50, 50), randi_range(0, -50)), Vector2i(1, 1)
+			)
+		Arm.look_at(MousePosition)
+		ArmRotation = Arm.rotation
+	else:
+		Arm.rotation = ArmRotation
 
 
 const mouse_sensitivity = 10
@@ -48,7 +58,9 @@ var MousePosition: Vector2
 
 
 func LeftMouseClicked():
-	Globals.WorldMap.MineCellAtPosition(get_global_mouse_position())
+	if(CurrentTool == 1):
+		
+		Globals.WorldMap.MineCellAtPosition(get_global_mouse_position())
 	pass
 
 
